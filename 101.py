e1e80a98b4da563ff719962e50b8fbeb342d5418
Gilvan F <100004547+GilvanCFilho@users.noreply.github.com>
2024-04-05 08:57:12 -0300

    Add files via upload

---
+++ 101.py
@@ -0,0 +1,38 @@
+""" 
+
+ print é uma function
+ type = function + class = str or int or bool;
+ type tem o objetivo de descobrir o tipo do objeto citado;
+ string ou str = text geralmente circulada pro "" ou '';
+ numeros dentro de print podem ser int , exemplo = -3,-2,-1,0,1,2,3;
+ numeros dentro de print/type tambem podem ser float , exemplos 1.1 , 1.2 , 1.3;
+ numeros sem sinal são considerados positivos o mesmo se aplica a int e float;
+ callable in python = é usada para verificar se um objeto pode ser chamado como uma função;
+ exemplo print / type;
+ boolean = dado binario para true or false;
+ totalizando str, int, float, bool (built in types / primitivos-imutaveis);
+
+""" 
+
+print(type(123))
+#ex int
+print(type('ola'))
+#ex string
+print(type(1.2))
+#ex float 
+print(10==10) #ex boolean true
+print(10==3) #ex boolean false
+
+print(type(10==10)) #ex type with boolean true (bool)
+
+print(int('1') + 3) #ex coearção de '1'(str) para int e somando com 3
+print(float('5') + 3) #ex para converção 
+
+
+"""
+
+Var são para salvar infos na memoria 
+#ex 
+
+"""
+      
