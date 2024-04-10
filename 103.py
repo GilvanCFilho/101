e13f642eb268d1ec6b829d06fc1371a007ba8514
Gilvan F <100004547+GilvanCFilho@users.noreply.github.com>
2024-04-10 09:31:26 -0300

    Create 103.py

---
+++ 103.py
@@ -0,0 +1,10 @@
+"""
+format outra forma de formatar strings , inserindo vars dentro delas
+"""
+
+a = '10'
+b = '20'
+c = 5.5
+formato = 'a={} b={} c={:.2f}'.format(a,b,c)
+
+print(formato)#ex de format
