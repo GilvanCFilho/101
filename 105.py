ccebbdbbec0e1310ad417c913a82e9fdff5e5473
Gilvan F <100004547+GilvanCFilho@users.noreply.github.com>
2024-04-15 10:21:42 -0300

    Add files via upload

---
+++ 105.py
@@ -0,0 +1,16 @@
+"""
+if / elif / else
+se / se não se / se não
+
+Estes são utilizados para construir estruturas condicionais, permitindo que o código tome decisões com base em condições específicas.
+"""
+
+
+entrada = input('Quer "entrar" ou "sair" ?')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema ')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else: 
+    print('ERROR componente não indentificado') #ex de  
