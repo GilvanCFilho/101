27db74f9fb41f3628f3cb5cee09ba891261c3746
Gilvan F <100004547+GilvanCFilho@users.noreply.github.com>
2024-04-15 10:09:31 -0300

    Add files via upload

---
+++ 104.py
@@ -0,0 +1,13 @@
+"""
+input é uma função usada para comunicação com o terminal
+"""
+
+numero_1 = input('Digite um numero')
+numero_2 = input('Digite outro')
+
+int_numero1 = int(numero_1)
+int_numero2 = int(numero_2)
+
+total = int_numero1 + int_numero2
+
+print(f'A soma dos dois é {total}')#calc feita em py via terminal(powershell) apenas com o basico 
