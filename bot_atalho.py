eeff8d2166f5e750817ae5054f7bcaed34989ada
Gilvan F <100004547+GilvanCFilho@users.noreply.github.com>
2024-05-16 11:49:07 -0300

    Delete bot_atalho.py

--- bot_atalho.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def executar_atalho(caminho_atalho):
-    os.startfile(caminho_atalho)
-
-if __name__ == "__main__":
-    caminho_do_atalho = r"C:\Users\Public\Desktop\Firefox.lnk"  # Substitua pelo caminho do seu atalho
-    executar_atalho(caminho_do_atalho)
